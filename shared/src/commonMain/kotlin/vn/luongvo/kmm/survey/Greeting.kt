41ec8bf0ad57a0833b6a1a1882ba56d1d83c3d50
Luong Vo (Lucas) <vodailuong@gmail.com>
2022-12-22 14:14:03 +0700

    [#7] Remove sample platform implementation

--- shared/src/commonMain/kotlin/vn/luongvo/kmm/survey/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package vn.luongvo.kmm.survey
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
