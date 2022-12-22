738aa4865bd863ddf906692f4c4daab13ae35100
Luong Vo (Lucas) <vodailuong@gmail.com>
2022-12-22 10:02:11 +0700

    [#7] Add LogInUseCaseTest

--- shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/test/MockUtil.kt
+++ shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/test/MockUtil.kt
@@ -1,9 +1,12 @@
 package vn.luongvo.kmm.survey.test
 
 import vn.luongvo.kmm.survey.data.remote.model.response.TokenResponse
+import vn.luongvo.kmm.survey.data.remote.model.response.toToken
 
 val tokenResponse = TokenResponse(
     tokenType = "tokenType",
     accessToken = "accessToken",
     refreshToken = "refreshToken"
 )
+
+val token = tokenResponse.toToken()
