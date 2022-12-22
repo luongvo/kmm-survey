332cfd5036267d38623ead34dd68551c7b224c39
Luong Vo (Lucas) <vodailuong@gmail.com>
2022-12-22 17:53:03 +0700

    [#7] Add necessary tests

--- shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/domain/usecase/LogInUseCaseTest.kt
+++ shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/domain/usecase/LogInUseCaseTest.kt
@@ -36,6 +36,11 @@ class LogInUseCaseTest {
             awaitItem() shouldBe token
             awaitComplete()
         }
+
+        verify(mockRepository)
+            .function(mockRepository::saveToken)
+            .with(eq(token))
+            .wasInvoked(exactly = 1.time)
     }
 
     @Test
@@ -51,5 +56,10 @@ class LogInUseCaseTest {
         useCase("email", "password").test {
             awaitError() shouldBe throwable
         }
+
+        verify(mockRepository)
+            .function(mockRepository::saveToken)
+            .with(any())
+            .wasNotInvoked()
     }
 }
