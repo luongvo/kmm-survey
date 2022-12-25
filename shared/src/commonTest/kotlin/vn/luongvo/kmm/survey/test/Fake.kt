1312b477285b96b93529c44b5623a06e3ec29c0d
Luong Vo (Lucas) <vodailuong@gmail.com>
2022-12-26 00:18:12 +0700

    [#8] Add ResponseMappingTest

--- shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/test/Fake.kt
+++ shared/src/commonTest/kotlin/vn/luongvo/kmm/survey/test/Fake.kt
@@ -1,9 +1,20 @@
 package vn.luongvo.kmm.survey.test
 
+import co.nimblehq.jsonapi.model.JsonApiError
+import co.nimblehq.jsonapi.model.JsonApiException
 import vn.luongvo.kmm.survey.data.remote.model.response.TokenResponse
 import vn.luongvo.kmm.survey.data.remote.model.response.toToken
 
 object Fake {
+
+    val jsonApiException = JsonApiException(
+        errors = listOf(
+            JsonApiError(
+                detail = "detail",
+                code = "code"
+            )
+        )
+    )
 
     val tokenResponse = TokenResponse(
         tokenType = "tokenType",
