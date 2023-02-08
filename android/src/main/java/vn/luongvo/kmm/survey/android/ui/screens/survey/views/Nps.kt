c0d539eb2e569726d64e195f6594804492fed4a2
Luong Vo (Lucas) <vodailuong@gmail.com>
2023-02-09 00:45:25 +0700

    [#27] Implement description texts on the bottom of selector

--- android/src/main/java/vn/luongvo/kmm/survey/android/ui/screens/survey/views/Nps.kt
+++ android/src/main/java/vn/luongvo/kmm/survey/android/ui/screens/survey/views/Nps.kt
@@ -8,10 +8,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import vn.luongvo.kmm.survey.android.R
 import vn.luongvo.kmm.survey.android.ui.screens.survey.AnswerUiModel
+import vn.luongvo.kmm.survey.android.ui.theme.AppTheme.dimensions
 import vn.luongvo.kmm.survey.android.ui.theme.AppTheme.shapes
 import vn.luongvo.kmm.survey.android.ui.theme.AppTheme.typography
 import vn.luongvo.kmm.survey.android.ui.theme.White50
@@ -23,41 +28,80 @@ fun Nps(
     modifier: Modifier = Modifier,
 ) {
     var selectedIndex by remember { mutableStateOf(-1) }
-    LazyRow(
+    ConstraintLayout(
         modifier = modifier
-            .border(BorderStroke(0.5.dp, White), shapes.medium)
-            .height(56.dp)
     ) {
-        items(answers.size) { index ->
-            val isSelected = index <= selectedIndex
-            Button(
-                onClick = {
-                    selectedIndex = index
-                    onValueChange(answers.getOrNull(selectedIndex)?.text.orEmpty())
-                },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Transparent
-                ),
-                elevation = null,
-                contentPadding = PaddingValues(0.dp),
-                modifier = Modifier
-                    .width(34.dp)
-                    .fillMaxHeight()
-            ) {
-                Text(
-                    text = "${index + 1}",
-                    color = if (isSelected) White else White50,
-                    style = if (isSelected) typography.h6 else typography.h6.copy(fontWeight = FontWeight.Normal)
-                )
-            }
-            if (index < answers.lastIndex) {
-                Spacer(
+        val (
+            itemsRef,
+            descriptionsRef,
+        ) = createRefs()
+
+        LazyRow(
+            modifier = Modifier
+                .border(BorderStroke(0.5.dp, White), shapes.medium)
+                .height(56.dp)
+                .wrapContentWidth()
+                .constrainAs(itemsRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            items(answers.size) { index ->
+                val isSelected = index <= selectedIndex
+                Button(
+                    onClick = {
+                        selectedIndex = index
+                        onValueChange(answers.getOrNull(selectedIndex)?.text.orEmpty())
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Transparent
+                    ),
+                    elevation = null,
+                    contentPadding = PaddingValues(0.dp),
                     modifier = Modifier
-                        .width(0.5.dp)
+                        .width(34.dp)
                         .fillMaxHeight()
-                        .background(White)
-                )
+                ) {
+                    Text(
+                        text = "${index + 1}",
+                        color = if (isSelected) White else White50,
+                        style = if (isSelected) typography.h6 else typography.h6.copy(fontWeight = FontWeight.Normal)
+                    )
+                }
+                if (index < answers.lastIndex) {
+                    Spacer(
+                        modifier = Modifier
+                            .width(0.5.dp)
+                            .fillMaxHeight()
+                            .background(White)
+                    )
+                }
             }
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(top = dimensions.paddingSmall)
+                .constrainAs(descriptionsRef) {
+                    top.linkTo(itemsRef.bottom)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                }
+        ) {
+            val isHalfLeftItemsSelected = selectedIndex < answers.size / 2
+            Text(
+                text = stringResource(id = R.string.nps_not_at_all_likely),
+                color = if (isHalfLeftItemsSelected) White else White50,
+                style = typography.body1.copy(fontWeight = FontWeight.Bold),
+            )
+            Text(
+                text = stringResource(id = R.string.nps_extremely_likely),
+                color = if (isHalfLeftItemsSelected.not()) White else White50,
+                style = typography.body1.copy(fontWeight = FontWeight.Bold),
+            )
         }
     }
 }
